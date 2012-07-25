a9ae4c8b9fafe4afae1408cadd47bf8bc5f599a6
Brandon Henricks <brandon.henricks1984@gmail.com>
2012-07-25 14:45:55 -0400

    SEOMozLib Base Solution
    Base Solution and start of Mozscape Class

---
+++ SEOMozLib/Classes/UrlMetrics.cs
@@ -0,0 +1,191 @@
+﻿using SEOMozLib.Interfaces;
+
+namespace SEOMozLib.Classes
+{
+    public class UrlMetrics : IUrlMetrics
+    {
+        #region IUrlMetrics Members
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>The URL.</value>
+        public string URL { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain.
+        /// </summary>
+        /// <value>The subdomain.</value>
+        public string Subdomain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain.
+        /// </summary>
+        /// <value>The root domain.</value>
+        public string RootDomain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the external links.
+        /// </summary>
+        /// <value>The external links.</value>
+        public int ExternalLinks { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain external links.
+        /// </summary>
+        /// <value>The subdomain external links.</value>
+        public int SubdomainExternalLinks { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain external links.
+        /// </summary>
+        /// <value>The root domain external links.</value>
+        public int RootDomainExternalLinks { get; set; }
+
+        /// <summary>
+        /// Gets or sets the juice passing links.
+        /// </summary>
+        /// <value>The juice passing links.</value>
+        public int JuicePassingLinks { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomains linking.
+        /// </summary>
+        /// <value>The subdomains linking.</value>
+        public int SubdomainsLinking { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domains linking.
+        /// </summary>
+        /// <value>The root domains linking.</value>
+        public int RootDomainsLinking { get; set; }
+
+        /// <summary>
+        /// Gets or sets the links.
+        /// </summary>
+        /// <value>The links.</value>
+        public int Links { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain subdomain linking.
+        /// </summary>
+        /// <value>The subdomain subdomain linking.</value>
+        public int SubdomainSubdomainLinking { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain root domain linking.
+        /// </summary>
+        /// <value>The root domain root domain linking.</value>
+        public int RootDomainRootDomainLinking { get; set; }
+
+        /// <summary>
+        /// Gets or sets the moz rank.
+        /// </summary>
+        /// <value>The moz rank.</value>
+        public int MozRank { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain moz rank.
+        /// </summary>
+        /// <value>The subdomain moz rank.</value>
+        public int SubdomainMozRank { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain moz rank.
+        /// </summary>
+        /// <value>The root domain moz rank.</value>
+        public int RootDomainMozRank { get; set; }
+
+        /// <summary>
+        /// Gets or sets the moz trust.
+        /// </summary>
+        /// <value>The moz trust.</value>
+        public int MozTrust { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain moz trust.
+        /// </summary>
+        /// <value>The subdomain moz trust.</value>
+        public int SubdomainMozTrust { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain moz trust.
+        /// </summary>
+        /// <value>The root domain moz trust.</value>
+        public int RootDomainMozTrust { get; set; }
+
+        /// <summary>
+        /// Gets or sets the external moz rank.
+        /// </summary>
+        /// <value>The external moz rank.</value>
+        public int ExternalMozRank { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain externa domain juice.
+        /// </summary>
+        /// <value>The subdomain externa domain juice.</value>
+        public int SubdomainExternaDomainJuice { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain external domain juice.
+        /// </summary>
+        /// <value>The root domain external domain juice.</value>
+        public int RootDomainExternalDomainJuice { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subdomain domain juice.
+        /// </summary>
+        /// <value>The subdomain domain juice.</value>
+        public int SubdomainDomainJuice { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domain domain juice.
+        /// </summary>
+        /// <value>The root domain domain juice.</value>
+        public int RootDomainDomainJuice { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HTTP status.
+        /// </summary>
+        /// <value>The HTTP status.</value>
+        public string HTTPStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the links to subdomain.
+        /// </summary>
+        /// <value>The links to subdomain.</value>
+        public int LinksToSubdomain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the links to root domain.
+        /// </summary>
+        /// <value>The links to root domain.</value>
+        public int LinksToRootDomain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root domains linking to sub domains.
+        /// </summary>
+        /// <value>The root domains linking to sub domains.</value>
+        public int RootDomainsLinkingToSubDomains { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page authority.
+        /// </summary>
+        /// <value>The page authority.</value>
+        public int PageAuthority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the domain authority.
+        /// </summary>
+        /// <value>The domain authority.</value>
+        public int DomainAuthority { get; set; }
+
+        #endregion
+    }
+}
---
+++ SEOMozLib/Mozscape.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SEOMozLib
+{
+    public enum MozAPI
+    {
+        URL_METRICS,
+        LINK_SCAPE
+    }
+
+    public class Mozscape
+    {
+        private string _mozAccessId;
+        private string _mozSecretKey;
+        private MozAPI _mozType;
+
+        public Mozscape()
+        {
+            this._mozAccessId = null;
+            this._mozSecretKey = null;
+            this._mozType = MozAPI.URL_METRICS;
+        }
+
+        public Mozscape(string strMozAccessId, string strMozSecretKey)
+        {
+            this._mozAccessId = strMozAccessId;
+            this._mozSecretKey = strMozSecretKey;
+        }
+
+        public Mozscape(string strMozAccessId, string strMozSecretKey, MozAPI mozType)
+        {
+            this._mozAccessId = strMozAccessId;
+            this._mozSecretKey = strMozSecretKey;
+            this._mozType = mozType;
+        }
+
+        public string CreateTimeStamp(int intHours)
+        {
+            var baseTime = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+
+            var expireTime = baseTime.Add(TimeSpan.FromHours(intHours));
+
+            var intTotalTime = expireTime.TotalSeconds;
+
+            var intUnixTimeStamp = (int) intTotalTime;
+
+            return intUnixTimeStamp.ToString(CultureInfo.InvariantCulture);
+
+        }
+
+        public string CreateAPISignature(string strMozAccessId, string strMozSecretKey)
+        {
+            return null;
+        }
+
+        public string CreateMozAPIUrl(string strUrl, MozAPI apiType, int intHours)
+        {
+            if (string.IsNullOrEmpty(strUrl)) return null;
+
+            var strApiUrl = string.Empty;
+            var expireTimeStamp = CreateTimeStamp(intHours);
+
+            switch (apiType)
+            {
+                    case MozAPI.URL_METRICS:
+                    break;
+
+                    case MozAPI.LINK_SCAPE:
+                    break;
+            }
+            return strApiUrl;
+        }
+
+        public string GetResults(string strUrl)
+        {
+            if (string.IsNullOrEmpty(strUrl)) return null;
+
+            var request = WebRequest.Create(strUrl);
+
+            using (var responseStream = request.GetResponse().GetResponseStream())
+            {
+                if (responseStream == null) return null;
+
+                var objReader = new StreamReader(responseStream);
+                var responseText = new StringBuilder();
+
+                string sLine = string.Empty;
+                int i = 0;
+
+                while (sLine != null)
+                {
+                    i++;
+                    sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        responseText.AppendLine(sLine);
+                }
+                return responseText.ToString();
+            }
+        }
+
+        public List<T> FormatResults<T>(string strResults)
+        {
+            return null;
+        }
+    }
+
+}
